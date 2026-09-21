2b7ea6c1f65d7a61d34951c6a9ed43e4b90fbea7
mosesvirus17 <mosesvirus17@gmail.com>
2026-09-21 11:45:59 +0100

    Add files via upload

---
+++ Work1.py
@@ -0,0 +1,46 @@
+  # 1. print ("Variable is a container that store value")
+  # 2.print("Through data type")
+  # 3.print("No")
+  # 4.age = 25 
+  # 5.name = "Kelechi"
+  # 6.x = 5; y = x; print(y)
+ 
+  
+  # 9.print("Yes")  
+  
+  # a=1,b=2,c=3
+  # a, b = 10, 20; print(a, b)
+  
+  # my_var
+  # no Name and name are not the same thing 
+  
+  
+  # X = 10; x = 20; print(x)
+
+school = "Noun"
+is_student = True
+name = "Attah"; print("name _ 2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a=10; b=20; a=b; print(a)
+# x="5"; y="10"; print(x+y)
+# x=5; y=10; print(x+y)
+# a=True; print(type(a))
+# num = 10; num = num +5; print(num)
+# a=5; a+=3; print(a)
+# a=10; b=a; b=20; print(a)
+# x,y = 5;
+# first_name = "John"; last_name="Doe"; print(first_name + last_name)
+# first_name = "John"; last_name="Doe"; print(first_name + "" + last_name)
