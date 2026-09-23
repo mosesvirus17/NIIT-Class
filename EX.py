be708217fd33d19e26afb603c09e07b0145ac9b3
mosesvirus17 <mosesvirus17@gmail.com>
2026-09-23 12:06:40 +0100

    Add files via upload

---
+++ EX.py
@@ -0,0 +1,180 @@
+if(100<50):
+ print(True)
+else:
+  print(False)
+
+if(200<10):
+  print(True)
+else:
+  print(False)
+
+if(50<10):
+  print(True)
+else:
+  print(False)
+
+if(20<10):
+  print(True)
+else:
+  print(False)
+
+if(10<1):
+  print(True)
+else:
+  print(False)
+
+if(50>2):
+  print(True)
+else:
+  print(False)
+
+if(50>100):
+  print(True)
+else:
+  print(False)
+
+if(100>20):
+  print(True)
+else:
+  print(False)
+
+if(50>90):
+  print(True)
+else:
+  print(False)
+
+if(100>200):
+  print(True)
+else:
+  print(False)
+
+if(200>=100):
+  print(True)
+else:
+  print(False)
+
+if(50>=2):
+  print(True)
+else:
+  print(False)
+
+if(50>=100):
+  print(True)
+else:
+  print(False)
+
+if(100>=20):
+  print(True)
+else:
+  print(False)
+
+if(50>=90):
+  print(True)
+else:
+  print(False)
+
+if(100<=500):
+ print(True)
+else:
+  print(False)
+
+if(200<=100):
+  print(True)
+else:
+  print(False)
+
+if(10<=50):
+  print(True)
+else:
+  print(False)
+
+if(20<=10):
+  print(True)
+else:
+  print(False)
+
+if(1<=10):
+  print(True)
+else:
+  print(False)
+
+
+if(20>0):
+  print("First")
+elif(5>10):
+ print("second")
+
+else:
+  print("Third")
+
+if(1>10):
+  print("First")
+elif(10>1):
+ print("second")
+
+else:
+  print("Third")
+
+if(5>10):
+  print("First")
+elif(5>10):
+ print("second")
+
+else:
+  print("Third")
+
+if(25>15):
+  print("First")
+elif(5>10):
+ print("second")
+
+else:
+  print("Third")
+
+if(9>=18):
+  print("First")
+elif(5<=1):
+ print("second")
+
+else:
+  print("Third")
+
+if(8>16):
+  print("First")
+elif(5>10):
+ print("second")
+
+else:
+  print("Third")
+
+if(5>=10):
+  print("First")
+elif(5<=10):
+ print("second")
+
+else:
+  print("Third")
+
+if(15<10):
+  print("First")
+elif(15<20):
+ print("second")
+
+else:
+  print("Third")
+
+if(15<10):
+  print("First")
+elif(25<10):
+ print("second")
+
+else:
+  print("Third")
+
+if(10>=20):
+  print("First")
+elif(5<=10):
+ print("second")
+
+else:
+  print("Third")
